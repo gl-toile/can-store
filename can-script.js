8edb6ed526c93a65228549afe87ec7181558d60e
gl-toile <larroque.guillaume@gmail.com>
2020-05-09 13:33:51 +0200

    Stream work ok

--- can-script.js
+++ can-script.js
@@ -12,43 +12,58 @@ request.onload = function(){
   if(request.status == 200 || request.readyState == 4){
     products = request.response;
     console.log(products);
-    afficherImages();
+    products.forEach(function(product){
+      getBlob(product);
+    });
   } else {
-    console.log("Erreur" + request.status + " : " + request.readyState);
+    console.log("Erreur" + request.status + " : " + request.statusText);
   }
 }
 request.send();
 
-function afficherImages(){
+function getBlob(product){
+  var url = 'images/' + product.image;
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.responseType = 'blob';
+
+  request.onload = function(){
+    if(request.status == 200 || request.readyState ==4){
+      console.log(request.response);
+      console.log(request.response instanceof Blob);
+      objectURL = URL.createObjectURL(request.response);
+      showProduct(objectURL, product);
+    } else {
+      console.log("ERREUR " + request.status + " ; " + request.statusText);
+    }
+  }
+  request.send();
+}
+
+function showProduct(objectURL, product){
+
   var can = document.createElement('section');
+  can.className = product.type;
   var myH2 = document.createElement('h2');
   var myPara = document.createElement('p');
   var myImg = document.createElement('img');
 
-  products.forEach(function(product){
-    var can = document.createElement('section');
-    can.className = product.type;
-    var myH2 = document.createElement('h2');
-    var myPara = document.createElement('p');
-    var myImg = document.createElement('img');
+  var sectionContent=[];
+  myH2.textContent = product.name.replace(product.name.charAt(0), product.name.charAt(0).toUpperCase());
+  console.log(`The character at index ${0} is ${product.name.charAt(0)}`);
+  myPara.textContent = '$' + product.price.toFixed(2);
+  myImg.src=objectURL;
 
-    var sectionContent=[];
-    myH2.textContent = product.name;
-    myPara.textContent = '$' + product.price;
-    myImg.src='./images/' + product.image;
+  sectionContent = [
+    myH2,
+    myPara,
+    myImg
+  ];
 
-    sectionContent = [
-      myH2,
-      myPara,
-      myImg
-    ];
+  for(let i=0; i < sectionContent.length ; i++){
+    can.appendChild(sectionContent[i]);
+  };
 
-     for(let i=0; i < sectionContent.length ; i++){
-       console.log(sectionContent[i]);
-       can.appendChild(sectionContent[i]);
-     };
-
-    main.appendChild(can);
-  });
+  main.appendChild(can);
 
 }
